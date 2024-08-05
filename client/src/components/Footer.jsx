b4ded8a6130daf4bf1b71e1c52c7c20c2b1e60bb
Mavine1 <mavinemalin@gmail.com>
2024-08-05 19:06:00 +0300

    middle inside by clothing method

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { Footer } from 'flowbite-react';
+import { BsFacebook, BsInstagram, BsTwitter,} from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+          </div>
+        </div>
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="David Inspiration"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+          <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+          <Footer.Link href='#'>Privacy Policy</Footer.Link>
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='#' icon={BsTwitter}/>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <div className='flex flex-col gap-4 p-4 mx-auto px-3 max-w-6xl '>
+        <h1 className='text-2xl '><em>Ignite Your Faith, Illuminate Your Path</em></h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+           Discover daily inspiration and spiritual insights to guide your journey
+        </p>
+      </div>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+  {posts && posts.length > 0 && (
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-4'>
+        {posts.slice(0, 3).map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </div>
+      <Link
+        to={'/search'}
+        className='text-lg text-teal-500 hover:underline text-center'
+      >
+        View all posts
+      </Link>
+    </div>
+  )}
+</div>
+    
+    </div>
+  );
+}
