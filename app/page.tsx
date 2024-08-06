56cf3c2aa7de4dd78169063865b4305b17ee6cb6
Mavine1 <mavinemalin@gmail.com>
2024-08-06 18:52:00 +0300

    shoulder measure including disease setting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative b">
+    <main className="relative bg-gray-300">
       <div>
         <h1>Hello world</h1>
       </div>
