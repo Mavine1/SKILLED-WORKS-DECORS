7fb584fecdc2e59e6877ace1205c74c34023aff9
Mavine1 <mavinemalin@gmail.com>
2024-08-06 19:30:01 +0300

    ten sport type eaten worried

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -5,7 +5,8 @@ const Hero = () => {
   return (
     <div className="pb-20 pt-36">
         <div >
-        <Spotlight classnam/>   
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+        fill="white"/>   
         </div>
     </div>
   )
