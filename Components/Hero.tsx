25ef5172545392509b79d13f78ad51c47e81241e
Mavine1 <mavinemalin@gmail.com>
2024-08-06 19:32:00 +0300

    dream swept disease familiar opinion

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -6,7 +6,11 @@ const Hero = () => {
     <div className="pb-20 pt-36">
         <div >
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-        fill="white"/>   
+        fill="blue"/>   
+        <Spotlight className="top-10 left-10 md:-left-32 md:-top-20 h-screen"
+        fill="blue"/>  
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+        fill="blue"/>  
         </div>
     </div>
   )
