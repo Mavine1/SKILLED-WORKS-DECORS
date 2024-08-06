1f3ae8241c37a8a0a857174d5686941b4dc0ab64
Mavine1 <mavinemalin@gmail.com>
2024-08-06 20:32:00 +0300

    sort very through cowboy iron

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/Components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-white-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
+    <main className="relative bg-blue-900 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
       <div className="max-w-10xl w-full">
         <Hero />
       </div>
