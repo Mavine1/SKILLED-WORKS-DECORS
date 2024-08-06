efaa494cdac5eaecac4e132c724eff05d0468a41
Mavine1 <mavinemalin@gmail.com>
2024-08-06 18:54:00 +0300

    already nuts system village friendly

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-gray-300">
+    <main className="relative bg-gray-100 flex justify-center items-center flex-col
+    ">
       <div>
         <h1>Hello world</h1>
       </div>
