7f1d0b1e4bcdc6d734f0aeaaf2829c57068e2680
Mavine1 <mavinemalin@gmail.com>
2024-08-06 20:02:00 +0300

    park using national farm sit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Hero from "@/Components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-white-100 flex justify-center items-center flex-col
-    overflow-hidden mx-auto sm:px-10 px-5">
-      <div className= "max-w-7xl w-full">
+    <main className="relative bg-white-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
+      <div className="max-w-7xl w-full">
         <Hero />
       </div>
     </main>
