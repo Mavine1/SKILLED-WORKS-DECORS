62f6a441ccb510a4620fa4404614c6a911283c69
Mavine1 <mavinemalin@gmail.com>
2024-08-06 19:54:00 +0300

    crowd highest explore silly fifty

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -19,16 +19,7 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* Grid */}
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
-      >
-        <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div>
+    
 
       {/* Content */}
       <div className="relative flex justify-center my-20 z-10">
