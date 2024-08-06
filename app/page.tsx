9d6f7e3aaaf2687d2109621b5e712f5227655243
Mavine1 <mavinemalin@gmail.com>
2024-08-06 20:50:00 +0300

    compare note chain direct excited

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/Components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-blue-900 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen" style={{ backgroundImage: 'url(https://images.pexels.com/photos/2291462/pexels-photo-2291462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)'>
+    <main className="relative bg-white-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
       <div className="max-w-10xl w-full">
         <Hero />
       </div>
