4f5245f2d444dcf55a5ce46776618cd6972d9b1c
Mavine1 <mavinemalin@gmail.com>
2024-08-06 20:14:00 +0300

    money over movement measure few

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/Components/Hero";
 export default function Home() {
   return (
     <main className="relative bg-white-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
-      <div className="w-full">
+      <div className="max-w-10xl w-full">
         <Hero />
       </div>
     </main>
