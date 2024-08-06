ed328a517924d8e7b0661772d01037de3a283901
Mavine1 <mavinemalin@gmail.com>
2024-08-06 20:12:00 +0300

    younger sale joined sale doctor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/Components/Hero";
 export default function Home() {
   return (
     <main className="relative bg-white-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
-      <div className="max-w-7xl w-full">
+      <div className="w-full">
         <Hero />
       </div>
     </main>
