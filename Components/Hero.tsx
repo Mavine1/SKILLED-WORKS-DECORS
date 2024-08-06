996905f62f5a3ff0cb6e0817ce0debd00f212249
Mavine1 <mavinemalin@gmail.com>
2024-08-06 19:34:00 +0300

    sunlight pass compound eye further

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div >
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
         fill="blue"/>   
-        <Spotlight className="top-10 left-10 md:-left-32 md:-top-20 h-screen"
+        <Spotlight className="top-10 left-full"
         fill="blue"/>  
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
         fill="blue"/>  
