173d6be2ad004cf2902fd99c55b7616108920cab
Mavine1 <mavinemalin@gmail.com>
2024-08-07 18:44:00 +0300

    blood plain seen combine milk

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,6 +1,7 @@
-// import { FaLocationArrow } from "react-icons/fa6";
 
-// import MagicButton from "./MagicButton";
+//import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
@@ -60,13 +61,13 @@ const Hero = () => {
           <em>Our client's satisfaction is our outermost happiness</em>
           </p>
 
-          {/* <a href="#about">
+          <a href="#about">
             <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
               position="right"
             />
-          </a> */}
+          </a>
         </div>
       </div>
     </div>
