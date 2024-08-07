86de1e3503d2755cfecdf178b8bafa9662d5a6d5
Mavine1 <mavinemalin@gmail.com>
2024-08-07 17:48:00 +0300

    twelve orange dot sink loud

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,11 @@
-// "use client";
-
 import Hero from "@/Components/Hero";
 
-// import { navItems } from "@/data";
-// import Grid from "@/components/Grid";
-// import Footer from "@/components/Footer";
-// import Clients from "@/components/Clients";
-// import Approach from "@/components/Approach";
-// import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
-// import { FloatingNav } from "@/components/ui/FloatingNavbar";
-
-const Home = () => {
+export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={navItems} /> */}
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
+      <div className="max-w-10xl w-full">
         <Hero />
-        
-        {/* <Grid />
-        <RecentProjects />
-        <Clients />
-        <Experience />
-        <Approach />
-        <Footer /> */}
       </div>
     </main>
   );
-};
-
-export default Home;
+}
