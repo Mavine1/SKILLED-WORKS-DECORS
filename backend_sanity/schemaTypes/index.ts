5c14c37fc96accc37542f38a089d4a4caa1e31f1
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:50:00 +0300

    size aboard yourself duck face

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -3,12 +3,14 @@ import brands from "./brands";
 import contact from "./contact";
 import DecorSample from "./DecorSample";
 import testimonials from "./testimonials";
+import workExperience from "./workExperience";
 
 export const schemaTypes = [
     brands,
     abouts,
     contact,
     testimonials,
-    DecorSample
+    DecorSample,
+    workExperience
 ]
 
