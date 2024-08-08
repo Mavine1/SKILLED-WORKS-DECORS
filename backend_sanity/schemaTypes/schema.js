25edfdc070c5454504ad03af46d4daa26b849bf9
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:36:00 +0300

    late cave giving busy agree

--- backend_sanity/schemaTypes/schema.js
+++ backend_sanity/schemaTypes/schema.js
@@ -3,7 +3,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import works from './works'
+import works from './DecorSample'
 import testimonials from './testimonials'
 import brands from './brands'
 import abouts from './abouts'
