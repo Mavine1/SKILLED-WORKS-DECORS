25ff86bae9fbf4e19647e1fdec19e484e150355b
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:40:00 +0300

    discuss cell edge affect poem

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -2,17 +2,12 @@ import abouts from "./abouts";
 import brands from "./brands";
 import contact from "./contact";
 import DecorSample from "./DecorSample";
-import experiences from "./experiences";
-import skills from "./skills";
 import testimonials from "./testimonials";
-import workExperience from "./workExperience";
 
 export const schemaTypes = [
     brands,
     abouts,
     contact,
-    experiences,
-    skills,
     testimonials,
     DecorSample
 
