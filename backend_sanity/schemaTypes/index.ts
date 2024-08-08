dbcb2ce54cdc7f4cbce1e3cee34aabedde1af4cc
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:32:00 +0300

    next full mail smoke widely

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -2,12 +2,17 @@ import abouts from "./abouts";
 import brands from "./brands";
 import contact from "./contact";
 import experiences from "./experiences";
+import skills from "./skills";
+import testimonials from "./testimonials";
 
 export const schemaTypes = [
     brands,
     abouts,
     contact,
     experiences,
+    skills,
+    testimonials,
     
+
 ]
 
