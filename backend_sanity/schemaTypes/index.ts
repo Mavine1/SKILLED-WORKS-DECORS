dc77f5b7b98de7228a9995b92f36745f4b83faf7
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:20:01 +0300

    coast satisfied consonant single occasionally

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -1,1 +1,20 @@
-export const schemaTypes = []
+export const schemaTypes = [
+    name:'brands',
+    title:'Brands',
+    type: 'document',
+    fields:[
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+        {
+            name:'name',
+            title:'Name',
+            type:'string'
+        }
+    ]
+]
