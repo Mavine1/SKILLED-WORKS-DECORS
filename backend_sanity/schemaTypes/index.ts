9b86958d5a6506d7f9790aede482a3a8027d9a0b
Mavine1 <mavinemalin@gmail.com>
2024-08-08 23:52:00 +0300

    sure sale solar youth cage

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -3,14 +3,12 @@ import brands from "./brands";
 import contact from "./contact";
 import DecorSample from "./DecorSample";
 import testimonials from "./testimonials";
-import workExperience from "./workExperience";
 
 export const schemaTypes = [
     brands,
     abouts,
     contact,
     testimonials,
-    DecorSample,
-    workExperience
+    DecorSample
 ]
 
