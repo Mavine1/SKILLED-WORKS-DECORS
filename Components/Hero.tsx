63fa08077027132a1aa4d29b1a73e60a96ed4d59
Mavine1 <mavinemalin@gmail.com>
2024-08-08 12:02:13 +0300

    command plan mind winter sail

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -7,7 +7,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-30 pt-36">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
