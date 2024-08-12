5eeae47b24a279071c9102a4deaefa5325c266c5
Mavine1 <mavinemalin@gmail.com>
2024-08-12 19:34:01 +0300

    train eleven corner musical leader

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2023-05-03',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'sksBzMpMbZias290rdlN9Py3uSAmovxSrg8R5SWSBmszYNE4YQvxstr7gpFFyqfj8Q46LA10aMoMuMVENp9p7eCaQp5n2DhRfeUOWBQl65cd5jG1dSfeuZxkQbXBapeTJWrJ76NXx6Lur38bEvaNzDwPjMXT5drGb7lkjpYSaOWVyc4RCNbN',
 });
 
 const builder = imageUrlBuilder(client);
