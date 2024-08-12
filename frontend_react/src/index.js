44515a0052943daa3689ad753e5b4a316bf15b32
Mavine1 <mavinemalin@gmail.com>
2024-08-12 18:50:00 +0300

    dance orange include built behavior

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
