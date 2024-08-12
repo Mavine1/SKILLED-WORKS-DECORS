3b60af4b9b96cd8d26af6ceabe3f5ccbac710562
Mavine1 <mavinemalin@gmail.com>
2024-08-12 19:02:01 +0300

    vote teach engine fifty brief

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-// import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-// import { Navbar } from './components';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
   <div className="app">
     <h1>Hello World!</h1>
-    {/* <Navbar />
+    <Navbar />
     <Header />
     <About />
     <Work />
     <Skills />
     <Testimonial />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
