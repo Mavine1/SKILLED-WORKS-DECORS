695c079e0e3b4cfb0cc78209df47dc77c84b99fb
Mavine1 <mavinemalin@gmail.com>
2024-08-12 20:44:01 +0300

    large society court motor us

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from '../ui/MagicButton';
+import { Spotlight } from '../ui/Spotlight';
+import { TextGenerateEffect } from '../ui/TextGenerateEffect';
 import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
@@ -18,6 +21,12 @@ const scaleVariants = {
 
 const Header = () => (
   <div className="app__header app__flex">
+    <div>
+      <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+      <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
+      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+    </div>
+    
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -65,6 +74,23 @@ const Header = () => (
         </div>
       ))}
     </motion.div>
+
+    <div className="hero-content">
+      <TextGenerateEffect
+        words="Transforming spaces, inside out. Dreams realized."
+        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+      />
+      <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+        <em>Our client's satisfaction is our utmost happiness</em>
+      </p>
+      <a href="#projects">
+        <MagicButton
+          title="Sample Of our Works"
+          icon={<FaLocationArrow />}
+          position="right"
+        />
+      </a>
+    </div>
   </div>
 );
 
