b29a42e07848a3d1f1fd741ad8b67e56db1bd561
Mavine1 <mavinemalin@gmail.com>
2024-08-15 16:26:00 +0300

    announced in grabbed practical turn

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'test', 'contact'].map((item) => (
+        {['home', 'about', 'work', 'Testimonial', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
