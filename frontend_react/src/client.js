6a84c1574bdb381271a21a77db3f79458aeb8f98
Mavine1 <mavinemalin@gmail.com>
2024-08-15 14:48:00 +0300

    town ate nervous excited needs

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: '4m1bn9c6',
-  dataset: 'production',
-  apiVersion: '2023-05-03',
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  dataset: process.env.REACT_APP_DATASET,
+  apiVersion: process.env.REACT_APP_API_VERSION,
   useCdn: true,
-  token: 'sksBzMpMbZias290rdlN9Py3uSAmovxSrg8R5SWSBmszYNE4YQvxstr7gpFFyqfj8Q46LA10aMoMuMVENp9p7eCaQp5n2DhRfeUOWBQl65cd5jG1dSfeuZxkQbXBapeTJWrJ76NXx6Lur38bEvaNzDwPjMXT5drGb7lkjpYSaOWVyc4RCNbN',
+  token: process.env.REACT_APP_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
