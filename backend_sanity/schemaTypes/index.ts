53c386d49d49615b43605707523321298fd8ac9c
Mavine1 <mavinemalin@gmail.com>
2024-08-15 15:46:00 +0300

    bank care state harbor temperature

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -1,13 +1,11 @@
 import abouts from "./abouts";
 import brands from "./brands";
-import contact from "./contact";
 import DecorSample from "./DecorSample";
 import testimonials from "./testimonials";
 
 export const schemaTypes = [
     brands,
     abouts,
-    contact,
     testimonials,
     DecorSample
 ]
